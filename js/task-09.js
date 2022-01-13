9eb6d34f2f3dde3bbe4091e31839d6c405747d8d
Kukovicya <92583408+Kukovicya@users.noreply.github.com>
2022-01-13 14:37:13 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,18 @@
 const form = document.querySelector(".change-color");
 
+const forma = document.querySelector(".color");
 
-
-const handleClick = event => {
-  console.log(event);
-  
+function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  
+}
+
+// console.log(color);
+const handleClick = (event) => {
+  let color = getRandomHexColor();
+  console.log(event);
+  forma.textContent = color;
+  document.body.style.background = color;
+  // document.bodyColor.style.backgroundColor = color;
 };
 
 form.addEventListener("click", handleClick);
