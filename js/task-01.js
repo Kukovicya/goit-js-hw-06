1bc478c47c2853f40d511468911dacf40108c98f
Kukovicya <92583408+Kukovicya@users.noreply.github.com>
2022-01-17 14:35:42 +0200

    funkcionalno

--- js/task-01.js
+++ js/task-01.js
@@ -1,38 +1,49 @@
 // let number  = 0;
 // if ul.classList.contains(cls)
-console.log(
-  `Number of categories:${document.querySelectorAll("li.item").length}`
-);
+// console.log(
+//   `Number of categories:${document.querySelectorAll("li.item").length}`
+// );
 
-console.log(
-  `Number of categories:${
-    document.querySelector("li.item").firstElementChild.textContent
-  }`
-);
-console.log(
-  `Number of categories:${
-    document.querySelector("li.item ul").childElementCount
-  }`
-);
+// console.log(
+//   `Number of categories:${
+//     document.querySelector("li.item").firstElementChild.textContent
+//   }`
+// );
+// console.log(
+//   `Number of categories:${
+//     document.querySelector("li.item ul").childElementCount
+//   }`
+// );
 
-console.log(
-  `Number of categories:${
-    document.querySelectorAll("li.item")[1].firstElementChild.textContent
-  }`
-);
-console.log(
-  `Number of categories:${
-    document.querySelectorAll("li.item ul")[1].childElementCount
-  }`
-);
+// console.log(
+//   `Number of categories:${
+//     document.querySelectorAll("li.item")[1].firstElementChild.textContent
+//   }`
+// );
+// console.log(
+//   `Number of categories:${
+//     document.querySelectorAll("li.item ul")[1].childElementCount
+//   }`
+// );
 
-console.log(
-  `Number of categories:${
-    document.querySelectorAll("li.item")[2].firstElementChild.textContent
-  }`
-);
-console.log(
-  `Number of categories:${
-    document.querySelectorAll("li.item ul")[2].childElementCount
-  }`
-);
+// console.log(
+//   `Number of categories:${
+//     document.querySelectorAll("li.item")[2].firstElementChild.textContent
+//   }`
+// );
+// console.log(
+//   `Number of categories:${
+//     document.querySelectorAll("li.item ul")[2].childElementCount
+//   }`
+// );
+const list = document.querySelectorAll("li.item");
+
+console.log(` Number of categories:${list.length} `);
+
+const ul = document.querySelectorAll("#categories>li");
+ul.forEach((el) => {
+  console.log(
+    `Category:${el.firstElementChild.textContent}
+Elements:${el.lastElementChild.children.length}`
+  );
+});
